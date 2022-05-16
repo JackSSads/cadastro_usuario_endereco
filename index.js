f6e47a254c7c3aeef08cb563023194cff6033f39
JackSSads <fotosjackosn2018@hotmail.com>
2022-05-16 20:39:46 -0300

    404 error handling

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const conn = require("./db/conn");
-const userRouter = require("./routers/userRouter");
 
 const app = express();
+
+const conn = require("./db/conn");
+const userRouter = require("./routers/userRouter");
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
@@ -20,6 +21,11 @@ app.use(express.static("public"));
 
 app.use("/", userRouter);
 
+app.use((req, res, next) => {
+    res.status(404).redirect("/error");
+});
+
 conn.sync()
     .then(app.listen(3000))
     .catch((err) => { console.log(err) });
+    
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const UserController = require("../controllers/UserController");
 const AddressController = require("../controllers/AddressController");
+const Error = require("../controllers/Error");
 
-/* User Routers */
+/* User Router */
 
 router.get("/", UserController.createUserHome);
 router.post("/add", UserController.createUserPost);
@@ -13,12 +14,14 @@ router.get("/users", UserController.getAllUsers);
 
 router.post("/user/delete", UserController.deleteUser);
 
-router.get("/user/edit/:id", UserController.editUser)
-router.post("/user/update", UserController.editUserPost)
+router.get("/user/edit/:id", UserController.editUser);
+router.post("/user/update", UserController.editUserPost);
 
-/* Address Routers */
-
+/* Address Router */
 router.post("/address/create", AddressController.newAddress);
 router.post("/address/delete", AddressController.deleteAddress);
+
+/* Error not found */
+router.get("/error", Error.errorNotFound);
 
 module.exports = router;
